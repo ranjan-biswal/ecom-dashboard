21d22eacf8c01440f1f60db0c06eccf86e274485
LEARNINGMATEMUM\priya.biswal <priya.biswal@learningmate.com>
2022-05-02 19:39:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>E-Com Project</h1>
+      <Button>Welcome</Button>
+    </div>
+  );
+}
+
+export default App;
